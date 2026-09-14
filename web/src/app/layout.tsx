1a2d7857e63c6b71fc6bd9d8621af6809443b3d9
laSolutions <lacp130688@gmail.com>
2026-09-14 15:06:56 -0400

    debug: instrumentation timestamps cleanup metas

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -28,7 +28,7 @@ export default function RootLayout({
           id="netlify-meta-cleanup"
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
-            __html: `(function(){try{var m=document.querySelectorAll('meta[name="hosting-provider"],meta[name="netlify-deploy"]');for(var i=0;i<m.length;i++){m[i].parentNode.removeChild(m[i]);}}catch(e){}})();`,
+            __html: `console.log('CLEANUP_START', Date.now());(function(){try{var m=document.querySelectorAll('meta[name="hosting-provider"],meta[name="netlify-deploy"]');for(var i=0;i<m.length;i++){m[i].parentNode.removeChild(m[i]);}}catch(e){console.log('CLEANUP_ERR', e.message)}})();console.log('CLEANUP_END', Date.now());`,
           }}
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
