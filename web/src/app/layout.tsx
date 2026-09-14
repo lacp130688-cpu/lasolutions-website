745d4598c8053c926e954ff10bcfaaab81558a0d
laSolutions <lacp130688@gmail.com>
2026-09-14 15:22:50 -0400

    chore: limpiar logs de debug del cleanup de metas

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -28,7 +28,9 @@ export default function RootLayout({
           id="netlify-meta-cleanup"
           strategy="beforeInteractive"
           dangerouslySetInnerHTML={{
-            __html: `console.log('CLEANUP_START', Date.now());(function(){try{var m=document.querySelectorAll('meta[name="hosting-provider"],meta[name="netlify-deploy"]');for(var i=0;i<m.length;i++){m[i].parentNode.removeChild(m[i]);}var n=document.head.childNodes;for(var i=n.length-1;i>=0;i--){if(n[i].nodeType===8&&n[i].textContent.indexOf('Netlify')!==-1)n[i].parentNode.removeChild(n[i]);}}catch(e){console.log('CLEANUP_ERR', e.message)}})();console.log('CLEANUP_END', Date.now());`,
+            __html: `(function(){try{var m=document.querySelectorAll('meta[name="hosting-provider"],meta[name="netlify-deploy"]');for(var i=0;i<m.length;i++){m[i].parentNode.removeChild(m[i]);}var n=document.head.childNodes;for(var i=n.length-1;i>=0;i--){if(n[i].nodeType===8&&n[i].textContent.indexOf('Netlify')!==-1)n[i].parentNode.removeChild(n[i]);}}catch(e){}})();`,
+          }}
+        />
           }}
         />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
